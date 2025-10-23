3766f9aff1173da0bb36cdbd68d7fb8ac5c854fb
Yevhenii Chornohorskyi <ychornohorskyi@INF.UG.EDU.PL>
2025-10-23 15:39:32 +0200

    poprawiam

--- src/account.py
+++ src/account.py
@@ -3,7 +3,7 @@ class Account:
         self.first_name = first_name
         self.last_name = last_name 
         self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid" 
-        self.balance = 50.0 if self.is_promo_valid(promo) and self.is_eligible_for_promo() else 0.0
+        self.balance = 50.0 if self.is_promo_valid(promo) and self.get_birth_year_from_pesel() < 65 else 0.0
     def is_pesel_valid(self,pesel):
         if isinstance(pesel,str) and (len(pesel) == 11):
             return True 
@@ -11,21 +11,12 @@ class Account:
         if promo and promo.startswith("PROM_") and len(promo) == 8:
             return True
     def get_birth_year_from_pesel(self): 
+        current_year = 2025
         if not self.is_pesel_valid(self.pesel):
             return None
-        year = int(self.pesel[:2])
-        month = int(self.pesel[2:4])
-        if month > 80: 
-            year += 1800
-        elif month > 60: 
-            year += 2200
-        elif month > 40: 
-            year += 2100
-        elif month > 20: 
-            year += 2000
+        year = int(self.pesel[:2]) 
+        if year > 30: 
+            year += 1900 
         else: 
-            year += 1900
-        return year 
-    def is_eligible_for_promo(self): 
-        birth_year = self.get_birth_year_from_pesel()
-        return birth_year is not None and birth_year > 1960
+            year += 2000
+        return current_year - year   
